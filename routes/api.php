fce7520b26db7b2da55e89059b6dedd16dd66fec
Md. Razun Mia <razun.neub.cse@gmail.com>
2026-04-16 03:48:27 -0700

    remove unused route

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('projects', App\Http\Controllers\ProjectsController::class);
-Route::apiResource('deployment-checks', App\Http\Controllers\DeploymentChecksController::class);
-
 Route::post('api/projects', [App\Http\Controllers\ProjectsController::class, 'store']);
 Route::get('api/projects', [App\Http\Controllers\ProjectsController::class, 'show']);   
 Route::post('api/projects/{project}/checks', [App\Http\Controllers\DeploymentChecksController::class, 'store']);
